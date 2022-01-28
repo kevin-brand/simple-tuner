c291d448adf99254e10a8c0c39585c4604a6fdf7
Kevin Brand <kevin-brand@outlook.com>
2022-01-28 16:33:23 +0100

    adjusted header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // Grab DOM-Elements
-const hamburger = document.querySelector('header .nav-list .hamburger');
-const mobileMenu = document.querySelector('header .nav-list ul');
-const navElements = document.querySelectorAll('header .nav-list ul a');
+const hamburger = document.querySelector('#header .nav-list .hamburger');
+const mobileMenu = document.querySelector('#header .nav-list ul');
+const navElements = document.querySelectorAll('#header .nav-list ul a');
+const header = document.querySelector('#header .header');
+
+let boxShadow;
+
+// Perform setup once DOM is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    boxShadow = window.getComputedStyle(header).getPropertyValue('--box-shadow');;
+});
+
 
 // Toggle active class to display the nav menu when someone clicks on the hamburger menu
 hamburger.addEventListener('click', () => {
@@ -16,4 +25,12 @@ navElements.forEach(e => {
         hamburger.classList.toggle('active');
         mobileMenu.classList.toggle('active');
     });
+});
+
+document.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    if (scrollPos > window.innerHeight - 10)
+      header.style.boxShadow = boxShadow;
+    else
+      header.style.boxShadow = 'none';
 });
