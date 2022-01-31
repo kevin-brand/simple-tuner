2beb604b26c77b4b22180e5d00d8026b4c146002
Kevin Brand <kevin-brand@outlook.com>
2022-01-31 16:47:44 +0100

    added dark theme toggle

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ const hamburger = document.querySelector('#header .nav-list .hamburger');
 const mobileMenu = document.querySelector('#header .nav-list ul');
 const navElements = document.querySelectorAll('#header .nav-list ul a');
 const header = document.querySelector('#header .header');
+const themeToggle = document.querySelector("#theme-toggle");
 
 let boxShadow;
 
 // Perform setup once DOM is fully loaded
 document.addEventListener('DOMContentLoaded', (event) => {
-    boxShadow = window.getComputedStyle(header).getPropertyValue('--box-shadow');;
+    boxShadow = window.getComputedStyle(header).getPropertyValue('--box-shadow');
 });
 
+themeToggle.addEventListener("click", () => {
+
+    setTimeout(function() {
+        boxShadow = window.getComputedStyle(header).getPropertyValue('--box-shadow');
+        header.style.boxShadow = boxShadow;
+    }, 100);
+
+    
+    document.body.classList.toggle('dark-theme-variables');
+});
 
 // Toggle active class to display the nav menu when someone clicks on the hamburger menu
 hamburger.addEventListener('click', () => {
